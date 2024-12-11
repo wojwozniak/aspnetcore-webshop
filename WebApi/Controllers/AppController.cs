232b7d916ade1c6e3886ee58ce917dfb99559ab5
Wojtek <pulchnymamut@gmail.com>
2024-12-11 15:22:59 +0100

    add AppController

---
+++ WebApi/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAppStatus()
+    {
+        return Ok(new { Message = "API is running." });
+    }
+}
