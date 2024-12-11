989fa08fc4dba432050feb098d4f911bf2d0e86f
Wojtek <pulchnymamut@gmail.com>
2024-12-11 15:05:43 +0100

    Setup database connection and swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddDbContext<ShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API v1");
+    });
 }
 
 app.UseHttpsRedirection();
