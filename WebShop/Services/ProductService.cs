7c470661fcce594ec01b2e7ae57eb96b63b7f90c
Wojtek <pulchnymamut@gmail.com>
2024-12-11 17:20:45 +0100

    create product service using generic service

---
+++ WebShop/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebClasses;
+
+namespace WebShop.Services
+{
+    public class ProductService
+    {
+        private readonly ApiService _apiService;
+        private const string ProductEndpoint = "products/";
+
+        public ProductService(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IEnumerable<WebClasses.Product>> GetProductsAsync()
+        {
+            return await _apiService.GetListAsync<WebClasses.Product>(ProductEndpoint);
+        }
+    }
+}
