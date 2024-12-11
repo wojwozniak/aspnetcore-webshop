fc4e2139ecdb0b73a62b6108137c23251fbd223c
Wojtek <pulchnymamut@gmail.com>
2024-12-11 15:06:01 +0100

    Setup test controller

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { Message = "Welcome to the Shop API" });
+    }
+}
