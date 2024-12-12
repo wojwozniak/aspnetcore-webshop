c82f17ee5b552dec9ec9e6c9c48ce99efe8baefd
Wojtek <pulchnymamut@gmail.com>
2024-12-12 20:24:51 +0100

    add authentication

--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using WebShop.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddControllersWithViews();
-
 
 services.AddHttpClient<HttpClient>(client =>
 {
@@ -15,25 +15,31 @@ services.AddHttpClient<HttpClient>(client =>
 
 var apiBaseUrl = builder.Configuration.GetValue<string>("ApiBaseUrl");
 services.AddSingleton(new ApiService(apiBaseUrl));
-
 services.AddScoped<ProductService>();
-
 
 services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "Google";
 })
 .AddCookie(options =>
 {
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
+})
+.AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
 });
-
 
 services.AddAuthorization(options =>
 {
     options.AddPolicy("RequireAuthenticated", policy =>
         policy.RequireAuthenticatedUser());
+
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireRole("Admin"));
 });
 
 var app = builder.Build();
@@ -44,12 +50,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.MapSwagger().RequireAuthorization();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapSwagger().RequireAuthorization();
 
 app.MapControllerRoute(
     name: "default",
