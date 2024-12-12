fbb7e6dd2ecdd195e2f19b570b976533158b52ca
Wojtek <pulchnymamut@gmail.com>
2024-12-12 20:50:30 +0100

    delete discounts

--- WebApi/Models/ShopDbContext.cs
+++ WebApi/Models/ShopDbContext.cs
@@ -13,7 +13,6 @@ namespace WebApi.Models
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Cart> Carts { get; set; }
-        public DbSet<Discount> Discounts { get; set; }
         public DbSet<Payment> Payments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
