91cf5f10f3a2d983d37ab03ff3c724c4b7a5909c
Wojtek <pulchnymamut@gmail.com>
2024-12-13 18:54:13 +0100

    setup base for acc service

---
+++ WebShop/Services/AccountService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebShop.Services
+{
+    public class AccountService
+    {
+        private readonly ApiService _apiService;
+
+        public AccountService(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task RegisterRequest(RegisterRequest request)
+        {
+            if (request == null)
+            {
+                return;
+            }
+
+            var res = _apiService.WebApi.RegisterAsync(body: request);
+            if(res is not null)
+            {
+
+            }
+        }
+
+        public async Task LoginRequest(LoginRequest request)
+        {
+            if (request == null)
+            {
+                return;
+            }
+
+            var res = _apiService.WebApi.LoginAsync(body: request);
+            if (res is not null)
+            {
+
+            }
+        }
+
+        public async Task GoogleAuth()
+        {
+            var res = _apiService.WebApi.LoginGoogleAsync();
+            if (res is not null)
+            {
+
+            }
+        }
+    }
+}
