9e678a4ed48bac9a7eec69aa973ddf603c2de090
Wojtek <pulchnymamut@gmail.com>
2024-12-13 19:41:05 +0100

    add jwt to apiservice

--- WebShop/Services/ApiService.cs
+++ WebShop/Services/ApiService.cs
@@ -1,16 +1,31 @@
-﻿namespace WebShop.Services
+﻿using System.Net.Http.Headers;
+
+namespace WebShop.Services
 {
     public class ApiService
     {
         private readonly WebApi _swaggerClient;
         private readonly HttpClient _httpClient;
 
-        public ApiService(string apiBaseUrl)
+        public ApiService(string apiBaseUrl, string jwtToken = null)
         {
             _httpClient = new HttpClient();
             _swaggerClient = new WebApi(apiBaseUrl, _httpClient);
+
+            if (!string.IsNullOrEmpty(jwtToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            }
         }
 
-        public WebApi WebApi { get { return _swaggerClient; } }
+        public WebApi WebApi => _swaggerClient;
+
+        public void SetJwtToken(string jwtToken)
+        {
+            if (!string.IsNullOrEmpty(jwtToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            }
+        }
     }
 }
