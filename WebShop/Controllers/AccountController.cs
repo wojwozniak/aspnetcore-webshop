c6cebf2441b509545fbd6f8a46a28173b71d61c4
Wojtek <pulchnymamut@gmail.com>
2024-12-13 19:35:02 +0100

    setup login

--- WebShop/Controllers/AccountController.cs
+++ WebShop/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.NetworkInformation;
 using WebShop.Services;
 
 namespace WebShop.Controllers
@@ -23,6 +22,25 @@ namespace WebShop.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                LoginResponse result = await _accountService.LoginRequest(request);
+
+                if (result is not null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Invalid login attempt. Please try again.");
+            }
+
+            return View(request);
         }
 
         [HttpPost]
